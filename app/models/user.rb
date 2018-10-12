ce5bba0f0937e4cb2817292419aae2c73a078c48
Pooja Kapuriya <pooja.kapuriya@neosofttech.com>
2018-10-12 14:04:38 +0530

    Made changes in root page design

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
-  def self.find_or_create_user(auth_hash)
+  def self.find_or_create_from_auth_hash(auth_hash)
     @user = User.find_or_create_by(
       provider: auth_hash[:provider],
       uid: auth_hash[:uid],
     )
     @user.update(
-      name: auth_hash[:info][:name] || auth_hash[:info][:nickname],
+      name: auth_hash[:info][:nickname],
       avatar_url: auth_hash[:info][:image],
       token: auth_hash[:credentials][:token],
     )
+
     return @user
   end
 end
