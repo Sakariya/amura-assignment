639e6ab4b475e23638f3f5aee23f9a4f4786829d
Pooja Kapuriya <pooja.kapuriya@neosofttech.com>
2018-10-14 09:48:13 +0530

    Written test cases for controller

---
+++ spec/controllers/repositories_controller_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe RepositoriesController, type: :controller do
+  # describe 'GET #index' do
+  #     let(:user) {FactoryBot.create(:auth_hash, :github)}
+  #     before(:each) do
+  #         @current_user = User.find_by(name: 'poojajk')
+  #     end
+
+  # 	it 'returns a success response' do
+  # 		get :index
+  # 		expect(response).to be_successful
+  # 	end
+  # end
+end
---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+  describe 'POST #create' do
+    context 'Success handling' do
+      before(:each) do
+        request.env['omniauth.auth'] = FactoryBot.create(:auth_hash, :github)
+        post :create, params: {provider: 'github'}
+      end
+      let(:user) { User.find_by(name: 'poojajk') }
+
+      it 'should set :notice flash' do
+        expect(flash[:notice]).to eq('Logged in successfully')
+      end
+
+      it 'should set current_user to proper user' do
+        expect(subject.current_user).to eq(user)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let(:user) {FactoryBot.create(:auth_hash, :github)}
+
+    before(:each) do
+      session[:user_id] =  user.id
+    end
+
+    it 'destroys the logged in user session' do
+      delete :destroy
+      expect(session).to be_empty
+    end
+
+    it 'redirects to the root page' do
+      delete :destroy
+      expect(response).to redirect_to(root_url)
+    end
+  end
+end
