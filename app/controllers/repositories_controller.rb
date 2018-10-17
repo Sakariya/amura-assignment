c1d7023326ffae58f4fe06f8879857f910bf363d
Pooja Kapuriya <pooja.kapuriya@neosofttech.com>
2018-10-17 12:25:26 +0530

    Made changes in test cases and fixed bugs

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,7 +6,7 @@ class RepositoriesController < ApplicationController
 
   def index
     # Call service for get all repositories
-    response = GithubApi.new(user: @current_user)
+    response = GithubApi.new(user: current_user)
     @repositories = response.repositories
   end
 
@@ -15,15 +15,16 @@ class RepositoriesController < ApplicationController
     @since_date = (Time.now - 5.day).beginning_of_day
     @until_date = Time.now
     # Call service for get repository detail
-    response = GithubApi.new(user: @current_user, repo_name: params[:id])
+    response = GithubApi.new(user: current_user, repo_name: params[:id])
     @repo = response.repository
+    redirect_to repositories_path, alert: 'Repo not found.' if @repo.nil?
   end
 
   def repo_commits
     @since_date = params[:since_date].to_date.beginning_of_day
     @until_date = params[:until_date].to_date.end_of_day
     # Call service for get repository commits history
-    response = GithubApi.new(user: @current_user, repo_name: params[:id],
+    response = GithubApi.new(user: current_user, repo_name: params[:id],
                              since_date: @since_date, until_date: @until_date)
     @commits = response.repo_commits
     render json: @commits
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,7 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require_relative 'support/controller_macros'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
@@ -43,17 +44,6 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
@@ -65,4 +55,6 @@ RSpec.configure do |config|
 
   # Turn on "test mode" for OmniAuth
   OmniAuth.config.test_mode = true
+  # exten macros
+  config.extend ControllerMacros, type: :controller
 end
